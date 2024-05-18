619e8f9d623ffedd847c93e5d8b0944dc46e168f
Derek Sutton <dereksutton86@gmail.com>
2024-05-17 21:37:46 -0400

    edited styles in Services component to optimize better for mobile viewports

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
-import Testimonials from './components/Testimonials';
 import Portfolio from './components/Portfolio';
+import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -23,3 +23,4 @@ function App() {
 }
 
 export default App;
+
