e176fc4db4b895362044d9cfe270c636cb0142e6
Derek Sutton <dereksutton86@gmail.com>
2024-11-06 10:11:08 -0500

    refined positioning and responsiveness of Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
         width: '100%',
-        height: '100vh',
+        minHeight: '100vh',
         position: 'relative',
     };
 
@@ -37,14 +37,19 @@ const Hero = () => {
                     <motion.img
                         src={logo}
                         alt="LED Custom Painting Logo"
-                        className="h-64 pl-0 lg:pl-8"
+                        className="h-72 pl-0 lg:pl-8"
                         initial={{ x: '-100vw' }}
                         animate={{ x: 0 }}
                         transition={{ type: 'spring', stiffness: 30 }}
                     />
-                    <nav className="hidden lg:flex">
+                    <nav className="hidden lg:flex flex-col items-end">
+                        <div className="text-yellow-500 poppins-semibold mb-2 pr-12"> {/* Phone number container */}
+                            <a href="tel:+19043850426" className="hover:text-white text-xl no-underline transition-colors duration-300">
+                                📞 (904) 385-0426
+                            </a>
+                        </div>
                         <motion.ul
-                            className="flex items-center space-x-20 pr-6 lg:pr-12 text-lg poppins-regular"
+                            className="flex items-center space-x-10 pr-6 text-lg poppins-regular"
                             initial={{ x: '100vw' }}
                             animate={{ x: 0 }}
                             transition={{ type: 'spring', stiffness: 30 }}
@@ -118,7 +123,7 @@ const Hero = () => {
                                 )}
                             </Menu>
                             <li><a href="#testimonials" className="hover:text-yellow-400 text-xl hover:no-underline">Testimonials</a></li>
-                            <li><a href="#quote" className="hover:text-yellow-400 text-xl hover:no-underline">Quote Request</a></li>
+                            <li><a href="#quote" className="bg-yellow-500 p-3 mr-10 rounded-md poppins-semibold text-slate-700 text-xl hover:no-underline hover:bg-yellow-300 hover:text-slate-700">Quote Request</a></li>
                         </motion.ul>
                     </nav>
                     <motion.div
@@ -139,7 +144,7 @@ const Hero = () => {
                     </motion.div>
                 </div>
                 {isMenuOpen && (
-                    <nav className="lg:hidden w-full">
+                    <nav className="lg:hidden w-full mb-10">
                         <ul className="flex flex-col items-center space-y-3 text-lg poppins-regular bg-slate-900 bg-opacity-75 brand-gold py-2">
                             <li><a href="#services" className="hover:text-white hover:no-underline">Services</a></li>
                             <li><a href="#about" className="hover:text-white hover:no-underline">About Us</a></li>
@@ -149,7 +154,7 @@ const Hero = () => {
                     </nav>
                 )}
                 <motion.div
-                    className="flex flex-col items-center justify-center h-full pb-64 sm:pb-48 lg:pb-64 px-4 lg:px-0 text-center w-full"
+                    className="flex flex-col items-center justify-center h-full pb-44 sm:pb-48 lg:pb-64 px-4 lg:px-0 text-center w-full"
                     initial={{ y: '100vh' }}
                     animate={{ y: 0 }}
                     transition={{ type: 'spring', stiffness: 50 }}
@@ -157,15 +162,21 @@ const Hero = () => {
                     <h1 className="brand-gold text-4xl sm:text-4xl lg:text-5xl poppins-semibold mt-0 sm:mt-0 lg:mt-36 mb-2">
                         Transform Your Space With Precision Painting
                     </h1>
-                    <p className="text-white text-xl sm:text-xl lg:text-2xl poppins-regular mb-4 sm:mb-5 lg:mb-6 pb-6">
+                    <p className="text-white text-xl sm:text-xl lg:text-3xl poppins-regular mb-4 sm:mb-5 lg:mb-6 pb-4">
                         Quality You Can See, Service You Can Trust.
                     </p>
+                    <p className="text-white text-xl sm:text-xl lg:text-2xl poppins-regular mb-4 sm:mb-5 lg:mb-6 text-center">
+                        <span className="block mb-2 mt-4">
+                            ⭐⭐⭐⭐⭐
+                        </span>
+                            Proudly Maintaining a 5-Star Rating on Google
+                    </p>
                     <a
-                        href="#quote" className="inline-flex h-12 sm:h-12 lg:h-12 animate-shimmer items-center justify-center rounded-md border border-yellow-400
-                        bg-[linear-gradient(110deg,#FFD700,45%,#FFA500,55%,#FFD700)] bg-[length:200%_100%] px-4 sm:px-5 lg:px-6 poppins-bold text-slate-700 text-3xl sm:text-3xl lg:text-3xl
+                        href="#quote" className="inline-flex h-12 sm:h-12 lg:h-14 animate-shimmer items-center justify-center rounded-md border border-yellow-400
+                        bg-[linear-gradient(110deg,#FFD700,45%,#FFA500,55%,#FFD700)] bg-[length:200%_100%] px-12 sm:px-12 lg:px-12 poppins-bold text-slate-700 text-3xl sm:text-3xl lg:text-5xl
                         transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105
                         focus:outline-none focus:ring-0 focus:ring-yellow-500 focus:ring-offset-2 focus:ring-offset-yellow-50
-                        shadow-lg hover:shadow-2xl hover:no-underline hover:text-bluenpm -700"
+                        shadow-lg hover:shadow-2xl hover:no-underline hover:text-slate-700 mt-1"
                     >
                         Get a Free Quote
                     </a>
