bccc8e04792d74b59ff521d6c38c1bcb427a63e2
Derek Sutton <dereksutton86@gmail.com>
2024-11-07 20:36:26 -0500

    resized the logo in the Hero component, per client request

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
                     <motion.img
                         src={logo}
                         alt="LED Custom Painting Logo"
-                        className="h-72 pl-0 lg:pl-8"
+                        className="h-84 pl-0 lg:pl-8"
                         initial={{ x: '-100vw' }}
                         animate={{ x: 0 }}
                         transition={{ type: 'spring', stiffness: 30 }}
