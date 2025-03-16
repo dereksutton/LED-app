fcaf060debb19b8aac69e90f0c89bc5fd9f41e5a
Derek Sutton <dereksutton86@gmail.com>
2025-03-16 14:30:19 -0400

    altered color to brand-gold for Follow Us message in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <footer className="bg-slate-500 text-white py-4">
             <div className="container mx-auto text-center px-4">
-                <p className="poppins-semibold text-white text-3xl">Follow Us On Social Media!</p>
+                <p className="poppins-semibold brand-gold text-3xl">Follow Us On Social Media!</p>
                     <ul className="flex justify-center space-x-12 mt-4">
                         <li>
                             <a href="https://www.facebook.com/ledcustompainting?mibextid=kFxxJD" className="hover:text-gray-400">
